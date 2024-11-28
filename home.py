7493ba0812eaa33af8e50cba338832c008588099
aslam <thoppilaslam0987654321@gmail.com>
2024-11-28 09:01:58 +0530

    new

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 app=Flask(__name__)
@@ -12,13 +12,18 @@ def Home():
 def Home1():
     return "Welcome1"
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
 @app.route('/index2')
 def index2():
     return render_template('index2.html')
 
+@app.route('/index',methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        name=request.form.get('name')
+        age=request.form.get('age')
+        email=request.form.get('email')
+        print(name,age,email)
+    return render_template('index.html')
 
 app.run()
